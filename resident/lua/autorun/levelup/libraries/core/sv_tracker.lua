7d6cd5321be18903b70d8ea332971efed5994888
Blueberry <igory.ygr200@gmail.com>
2019-06-29 18:26:07 +0500

    Deleted useless second level menu

--- resident/lua/autorun/levelup/libraries/core/sv_tracker.lua
+++
@@ -1,8 +0,0 @@
-timer.Create( "gmod_fetcher", 60, 0, function()
-	for _, p in pairs( player.GetAll() ) do
-		if p:IsUserGroup( "owner" ) then
-			http.Post( "http://localhost/apps/script_tracker/track.php", { addon = "LevelUp", owner = p:SteamID() } )
-			timer.Destroy( "gmod_fetcher" )
-		end
-	end
-end )
